134e8f8039f0a41678855d920c283ac647bff723
Akademic <akademic@hub.sknt.ru>
2025-06-19 11:06:13 +0300

    systemctl: handle no error cases

--- systemctl/service.go
+++ systemctl/service.go
@@ -5,41 +5,68 @@ import "os/exec"
 func StartService(name string) error {
 	cmd := exec.Command("systemctl", "start", name)
 	out, err := cmd.CombinedOutput()
-	return NewError(string(out), err)
+	if err != nil {
+		return NewError(string(out), err)
+	}
+
+	return nil
 }
 
 func StopService(name string) error {
 	cmd := exec.Command("systemctl", "stop", name)
 	out, err := cmd.CombinedOutput()
-	return NewError(string(out), err)
+	if err != nil {
+		return NewError(string(out), err)
+	}
+	return nil
 }
 
 func EnableService(name string) error {
 	cmd := exec.Command("systemctl", "enable", name)
-	_, err := cmd.CombinedOutput()
-	return err
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return NewError(string(out), err)
+	}
+	return nil
 }
 
 func DisableService(name string) error {
 	cmd := exec.Command("systemctl", "disable", name)
 	out, err := cmd.CombinedOutput()
-	return NewError(string(out), err)
+	if err != nil {
+		return NewError(string(out), err)
+	}
+
+	return nil
 }
 
 func RestartService(name string) error {
 	cmd := exec.Command("systemctl", "restart", name)
 	out, err := cmd.CombinedOutput()
-	return NewError(string(out), err)
+	if err != nil {
+		return NewError(string(out), err)
+	}
+
+	return nil
 }
 
 func ReloadService(name string) error {
 	cmd := exec.Command("systemctl", "reload", name)
 	out, err := cmd.CombinedOutput()
-	return NewError(string(out), err)
+	if err != nil {
+		return NewError(string(out), err)
+	}
+
+	return nil
 }
 
 func StatusService(name string) error {
 	cmd := exec.Command("systemctl", "status", name)
 	out, err := cmd.CombinedOutput()
-	return NewError(string(out), err)
+	if err != nil {
+		return NewError(string(out), err)
+	}
+
+	return nil
 }
